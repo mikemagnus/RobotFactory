fff92880134c87eb4aa34118e253c7dcc1f46a39
Byron Henze <byronh@gmail.com>
2012-01-28 03:11:25 -0700

    Fixing linker error

---
+++ RobotFactory/RobotFactory/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  RobotFactory
+//
+//  Created by Christopher Lee on 12-01-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface GameScene : CCLayer
+
+@end
---
+++ RobotFactory/RobotFactory/GameScene.m
@@ -0,0 +1,13 @@
+//
+//  GameScene.m
+//  RobotFactory
+//
+//  Created by Christopher Lee on 12-01-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+@end
