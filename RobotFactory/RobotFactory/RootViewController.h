ad4d68086f8af379a77f3899d74f95cd5107ec98
Mike Magnus <mikemagnus@gmail.com>
2012-01-27 23:56:04 -0700

    Trying a new project

---
+++ RobotFactory/RobotFactory/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RobotFactory
+//
+//  Created by Michael Magnus on 12-01-27.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
